397a7862b3b1e1873b625606e7304507ec6ed8d3
Jiantao Hu <jh2468@cornell.edu>
2020-05-25 07:28:43 +0900

    wk8 finished

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -18,11 +18,12 @@ public class BaseballElimination {
 	private final Map<Integer, String> teamMapRev;
 	private final int[] w, l, r;
 	private final int[][] g;
+
 	public BaseballElimination(String filename){
 		// create a baseball division from given filename in format specified below
+		if (filename==null || filename.equals("")) throw new IllegalArgumentException();
 		In in  = new In(filename);
-		String[] input = in.readAllLines();
-		teamNum = Integer.parseInt(input[0].trim());
+		teamNum = Integer.parseInt(in.readLine().trim());
 		teams = new ArrayList<>();
 		teamMap = new HashMap<>();
 		teamMapRev = new HashMap<>();
@@ -30,28 +31,20 @@ public class BaseballElimination {
 		l = new int[teamNum];
 		r = new int[teamNum];
 		g = new int[teamNum][teamNum];
-		for (int i=1; i<input.length;i++){
-			String[] teamStat = input[i].trim().split(" ");
-			int j = 0;
-			for (String stat : teamStat){
-				if (!stat.equals("")){
-					if (j==0) {
-						teams.add(stat);
-						teamMap.put(stat,i-1);
-						teamMapRev.put(i-1, stat);
-					}
-					else if (j==1) w[i-1] = Integer.parseInt(stat);
-					else if (j==2) l[i-1] = Integer.parseInt(stat);
-					else if (j==3) r[i-1] = Integer.parseInt(stat);
-					else {
-						g[i-1][j-4] = Integer.parseInt(stat);
-					}
-					j++;
-				}
+		for (int i=0; i<teamNum;i++){
+			String[] stat = in.readLine().trim().split("\\s+");
+			teams.add(stat[0]);
+			teamMap.put(stat[0],i);
+			teamMapRev.put(i, stat[0]);
+			w[i] = Integer.parseInt(stat[1]);
+			l[i] = Integer.parseInt(stat[2]);
+			r[i] = Integer.parseInt(stat[3]);
+			for (int j=0;j<teamNum;j++){
+				g[i][j] = Integer.parseInt(stat[j+4]);
 			}
-
 		}		
 	}
+	
 	public int numberOfTeams(){
 		// number of teams
 		return teamNum;
@@ -72,17 +65,20 @@ public class BaseballElimination {
 		this.validate(team);
 		return l[this.teamMap.get(team)];
 	}
+	
 	public int remaining(String team){
 		// number of remaining games for given team
 		this.validate(team);
 		return r[this.teamMap.get(team)];
 	}
+	
 	public int against(String team1, String team2){
 		// number of remaining games between team1 and team2
 		this.validate(team1);
 		this.validate(team2);
 		return g[this.teamMap.get(team1)][this.teamMap.get(team2)];
 	}
+	
 	public boolean isEliminated(String team){
 		// is given team eliminated?
 		this.validate(team);
@@ -106,7 +102,7 @@ public class BaseballElimination {
 				gToV[i]= numG + i;
 			}
 		}
-		FlowNetwork fn = new FlowNetwork(numG+this.numberOfTeams()+1);
+		FlowNetwork fn = new FlowNetwork(numG + this.numberOfTeams() + 1);
 		int countGm = 1;
 		// first buildGameE
 		for (int i=0;i<this.numberOfTeams();i++){
@@ -116,8 +112,8 @@ public class BaseballElimination {
 				int capacity = g[i][j];
 				FlowEdge from = new FlowEdge(0,countGm,capacity);
 				fn.addEdge(from);
-				FlowEdge to1 = new FlowEdge(countGm, gToV[i], Integer.MAX_VALUE);
-				FlowEdge to2 = new FlowEdge(countGm, gToV[j], Integer.MAX_VALUE);
+				FlowEdge to1 = new FlowEdge(countGm, gToV[i], Double.POSITIVE_INFINITY);
+				FlowEdge to2 = new FlowEdge(countGm, gToV[j], Double.POSITIVE_INFINITY);
 				fn.addEdge(to1);
 				fn.addEdge(to2);
 				countGm++;
@@ -125,29 +121,29 @@ public class BaseballElimination {
 		}
 		// next build teamE
 		int teamMaxWin = this.wins(team) + this.remaining(team); 
-		for (int i=0;i<this.numberOfTeams()-1;i++){
+		for (int i=0;i<this.numberOfTeams();i++){
 			if (i==index) continue;
 			int capacity = teamMaxWin - this.w[i];
-			FlowEdge fe = new FlowEdge(gToV[i], numG+this.numberOfTeams(), capacity>0?capacity:0);
+			if (capacity<0){
+				res.add(this.teamMapRev.get(i));
+				return res;
+			}
+			FlowEdge fe = new FlowEdge(gToV[i], numG+this.numberOfTeams(), capacity);
 			fn.addEdge(fe);
 		}
 		FordFulkerson ff = new FordFulkerson(fn, 0, numG+this.numberOfTeams());
-		int vCounter = 1;
 		for (int i=0;i<this.numberOfTeams();i++){
-			for (int j=i+1;j<this.numberOfTeams();j++){
-				if (i==index) continue;
-				if (j==index) continue;
-				if (ff.inCut(vCounter)){
-					res.add(this.teamMapRev.get(i));
-				}
-				vCounter++;
+			if (i==index) continue;
+			if (ff.inCut(gToV[i])){
+				res.add(this.teamMapRev.get(i));
 			}
 		}
 		return res.isEmpty()?null:res;
 	}
 
-	private void validate(Object o){
-		if (o==null) throw new IllegalArgumentException();
+	private void validate(String team){
+		if (team==null) throw new IllegalArgumentException();
+		if (!teamMap.containsKey(team)) throw new IllegalArgumentException();
 	}
 
 	public static void main(String[] args) {
